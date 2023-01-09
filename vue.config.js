bcc1e14f3a4deaab53f9631bc03a30b6947053f3
polariis <polariis@163.com>
2023-01-09 23:44:42 +0800

    设置跨域

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,24 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: "./" //修改资源的对应路径
-});
+const path = require("path");
+
+module.exports = {
+  outputDir: "./build",
+  // publicPath: './',
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "http://152.136.185.210:4000",
+        pathRewrite: {
+          "^/api": ""
+        },
+        changeOrigin: true
+      }
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: "@/components"
+      }
+    }
+  }
+};
